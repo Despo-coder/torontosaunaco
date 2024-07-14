3234a325bf91adff172ecd61da432546047e4e2a
Despo-coder <damion.wilson@gmail.com>
2024-07-14 01:32:29 -0400

    Added Barrel Page

---
+++ components/Barrel.jsx
@@ -0,0 +1,40 @@
+import ProductCard from "./ProductCard"
+import Link from "next/link"
+import { Button } from "./ui/button"
+import {fetchProducts} from "@/assets/utils/request"
+
+
+
+
+const BarrelPage = async () => {
+
+    const product = await fetchProducts()
+   const cubes = product.filter(product => product.type === "Barrel")
+    const recentProducts = cubes.sort(()=> Math.random())
+    // .slice(0, )
+     return (
+    <>
+    <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto">
+           
+            <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-8">
+{recentProducts === 0 ? (<p>No Products found</p>):
+recentProducts.map((product, i)=>(
+    <ProductCard  key={i} product= {product} />
+))
+
+}
+            </div>
+        </div>
+     
+    </section>
+    <section className="m-auto max-w-lg my-10 px-6">
+<Link href="/saunas">
+    <Button className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View All Products</Button>
+</Link>
+    </section>
+    </>
+  )
+}
+
+export default BarrelPage
