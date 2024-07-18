c87a6adc9acd1949e9038824ebbde2cd6fe67fe3
Despo-coder <damion.wilson@gmail.com>
2024-07-18 19:51:11 -0400

    updated middleware

--- middleware.jsx
+++ middleware.jsx
@@ -1,36 +1,33 @@
 import { NextResponse } from 'next/server';
 import { getToken } from "next-auth/jwt";
-export {default} from 'next-auth/middleware'
+
+export { default } from 'next-auth/middleware';
 
 export async function middleware(request) {
     const token = await getToken({ req: request });
-    
-    // console.log('Current User:', token ? 'Authenticated' : 'Not Authenticated');
-    // console.log('Requested URL:', request.nextUrl.pathname);
-  
-    if (token && request.nextUrl.pathname === '/signin') {
-      return NextResponse.redirect(new URL('/', request.url));
+    const { pathname } = request.nextUrl;
+
+    if (token && pathname === '/signin') {
+        return NextResponse.redirect(new URL('/', request.url));
     }
-  
-    if (!token && !request.nextUrl.pathname.startsWith('/signin') && request.nextUrl.pathname !== '/') {
-      const signInUrl = new URL('/signin', request.url);
-      signInUrl.searchParams.set('callbackUrl', request.url);
-      return NextResponse.redirect(signInUrl);
+
+    if (!token && pathname !== '/' && !pathname.startsWith('/signin')) {
+        const signInUrl = new URL('/signin', request.url);
+        signInUrl.searchParams.set('callbackUrl', request.url);
+        return NextResponse.redirect(signInUrl);
     }
-  
+
     return NextResponse.next();
-  }
-  
+}
 
 export const config = {
-  matcher: [
-    '/saunas/add', 
-    '/products/edit', 
-    '/profile', 
-    '/messages', 
-    '/profile',
-    '/orders',
-    '/products/saved',
-    '/signin' // Include the sign-in page to handle redirection to home if already logged in
-  ],
+    matcher: [
+        '/saunas/add',
+        '/products/edit',
+        '/profile',
+        '/messages',
+        '/orders',
+        '/products/saved',
+        '/signin'
+    ],
 };
