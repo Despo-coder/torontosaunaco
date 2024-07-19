c4febfc85045aa637ad0d22e41640903cf6123d2
Despo-coder <damion.wilson@gmail.com>
2024-07-18 20:42:44 -0400

    updated middleware, added more server side logic

--- app/orders/page.jsx
+++ app/orders/page.jsx
@@ -1,23 +1,41 @@
-import React from 'react'
-import { getServerSession } from "next-auth/next"
-import { authOptions } from "@/assets/utils/authOptions"
-import OrderCard from '../../components/OrdersCard'
-import { fetchOrders } from '@/assets/utils/request'
+import React from 'react';
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/assets/utils/authOptions";
+import OrderCard from '../../components/OrdersCard';
+import { fetchOrders } from '@/assets/utils/request';
 
-const Page = async () => {
-  const session = await getServerSession(authOptions)
-  const orders = await fetchOrders()
+const OrdersPage = async () => {
+  // Get the user's session on the server side
+  const session = await getServerSession(authOptions);
 
-  const userOrders = session ? orders.filter(order => order.id === session.user.id) : []
+  if (!session) {
+    // Redirect to sign in page if there's no session
+    return (
+      <div>Please sign in to view your orders.</div>
+    );
+  }
 
-//   console.log(session.user.id)
-//   console.log(orders.id)
+  // Fetch orders on the server side
+  const orders = await fetchOrders();
+
+  // Filter orders for the logged-in user
+  const userOrders = orders.filter(order => order.id === session.user.secondaryId);
+
+
+
+
+  // Handle case when there are no orders
+  if (!userOrders.length) {
+    return (
+      <div>No orders found.</div>
+    );
+  }
 
   return (
     <div>
       <OrderCard orders={userOrders} />
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default OrdersPage;
