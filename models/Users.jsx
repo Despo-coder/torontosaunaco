f34659e60d0e4de8a02c78071a86b38b95614ad4
Derescio <ddw.web.dev.services@gmail.com>
2025-02-01 15:40:12 -0500

    Updated Email Issues

--- models/Users.jsx
+++ models/Users.jsx
@@ -1,26 +1,30 @@
 import { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema({
+    
     username: {
         type: String,
-        
     },
+    
     email: {
         type: String,
         required: true,
         unique: true,
     },
+    
     password: {
         type: String,
-        
     },
+   
     isAdmin: {
         type: Boolean,
         default: false,
     },
+    
     image: {
         type: String,
     },
+   
     bookmark:[{
         type: Schema.Types.ObjectId,
         ref: "Products",
