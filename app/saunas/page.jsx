f3c9bbf2dd26e62544b159e516aac22c6537e1b6
Despo-coder <damion.wilson@gmail.com>
2025-02-24 18:54:09 -0500

    Removed Cold Plunge from Saunas

--- app/saunas/page.jsx
+++ app/saunas/page.jsx
@@ -12,7 +12,10 @@ export const metadata = {
 
 const Saunas = async() => {
 const products = await fetchProducts()
-  return (
+
+const filteredProducts = Object.values(products).filter((x) => x.type !== 'Cold Plunge');
+
+return (
    <div>
       <section className="px-4 py-6 mt-6">
         
@@ -22,7 +25,7 @@ const products = await fetchProducts()
       <div className="container-xl lg:container m-auto px-4 py-6">
         {products.length === 0 ? (<p>No products found</p>) :(
     <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-8">
-      {products.map((product, index) => (
+      {filteredProducts.map((product, index) => (
       <ProductCard product={product} key={index} /> 
   ))}
   </div>
